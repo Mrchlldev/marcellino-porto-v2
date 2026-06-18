6be38d50f9eca620f173be4fddab68d3eee2cbfa
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-18 19:57:12 +0700

    Upload app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { AboutSection, ContactSection, EducationSection, HeroSection, MarqueeSection, ProjectSection, SkillSection } from "@/components/sections";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <MarqueeSection />
+      <AboutSection />
+      <SkillSection />
+      <EducationSection />
+      <ProjectSection />
+      <ContactSection />
+    </>
+  );
+}
