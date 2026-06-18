1a2a31cc8ff2e8b8526956c0dd159476a5238eea
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-18 19:57:31 +0700

    Upload lib/settings/navigation.ts

---
+++ lib/settings/navigation.ts
@@ -0,0 +1,8 @@
+export const navigationSettings = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Skill", href: "/skill" },
+  { label: "Education", href: "/education" },
+  { label: "Project", href: "/project" },
+  { label: "Contact", href: "/contact" }
+];
